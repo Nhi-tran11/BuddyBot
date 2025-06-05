d3e2cac59f806b679ae77d23f124cb8a019a0cbf
adiaut <adiaut24@gmail.com>
2025-06-05 22:24:31 +1200

    [after fixing errors]Made some frontend changes for the error message that is throwed when user tries to enter a subject in an already full slot. It throws a red error message that vibrates to catch attention and vanishes after a few seconds automatically

--- src/page/TimeTable.jsx
+++ src/page/TimeTable.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './timetable.css';
+import './TimeTable.css';
 
 const Timetable = () => {
   const [timetable, setTimetable] = useState([]);
