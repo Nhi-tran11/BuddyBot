2d1a8db429d8ac152f1d97b3486cc0421c21ca62
Ashwin Kasoju <ashwinsai8854@gmail.com>
2025-06-06 15:55:30 +1200

    Added storing attempts by subject feature. Also, leaderboard filtering by subject (dropdown to sort scores by subject) '

--- backend/routes/scoreRoutes.js
+++ backend/routes/scoreRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { submitScore, getLeaderboard, getLeaderboardBySubject } = require('../controllers/scoreController');
 
-const { submitScore, getLeaderboard } = require('../controllers/scoreController');
 
 router.post('/', submitScore);
 router.get('/leaderboard', getLeaderboard);
+router.get('/leaderboard/:subject', getLeaderboardBySubject); // by subjwct
 
 module.exports = router;
